c2ba7bddaeb9d2806b76a664248a37dee9b0fbd2
Zhukov Andrey <zhukov_av@taximaxim.ru>
2019-12-13 16:03:16 +0500

    Добавил ClientId (это id бота) и ChannelId

--- StriBot/TwitchInfo.cs
+++ StriBot/TwitchInfo.cs
@@ -6,8 +6,15 @@ namespace StriBot
 {
     internal class TwitchInfo
     {
+        internal readonly string Channel;
+        internal readonly string AccessToken;
+        internal readonly string BotName;
+        internal readonly string ClientId;
+        internal readonly string ChannelId;
+
         private string fileName = "TwitchInfo.txt";
         private string error = "Error";
+
         internal TwitchInfo()
         {
             try
@@ -19,6 +26,8 @@ namespace StriBot
                     Channel = line[0];
                     AccessToken = line[1];
                     BotName = line[2];
+                    ClientId = line[3];
+                    ChannelId = line[4];
                 }
             }
             catch (IOException e)
@@ -30,9 +39,5 @@ namespace StriBot
                 MessageBox.Show($"{fileName} {e.Message}", error);
             }
         }
-
-        internal readonly string Channel;
-        internal readonly string AccessToken;
-        internal readonly string BotName;
     }
 }
